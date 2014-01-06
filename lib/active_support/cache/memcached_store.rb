d64079cfe65520a560bcf9c3a64253b83e5844cb
Arthur Neves <arthurnn@gmail.com>
2014-01-06 14:22:31 -0500

    Add memcached AS store

---
+++ lib/active_support/cache/memcached_store.rb
@@ -0,0 +1,152 @@
+require 'digest/md5'
+require 'active_support/core_ext/string/encoding'
+
+module ActiveSupport
+  module Cache
+    class MemcachedStore < Store
+      FATAL_EXCEPTIONS = [ Memcached::ABadKeyWasProvidedOrCharactersOutOfRange,
+        Memcached::AKeyLengthOfZeroWasProvided,
+        Memcached::ConnectionBindFailure,
+        Memcached::ConnectionDataDoesNotExist,
+        Memcached::ConnectionFailure,
+        Memcached::ConnectionSocketCreateFailure,
+        Memcached::CouldNotOpenUnixSocket,
+        Memcached::NoServersDefined,
+        Memcached::TheHostTransportProtocolDoesNotMatchThatOfTheClient
+      ]
+
+#      if defined?(::Rails) && ::Rails.env.test?
+#        NONFATAL_EXCEPTIONS = []
+#      else
+      NONFATAL_EXCEPTIONS = Memcached::EXCEPTIONS - FATAL_EXCEPTIONS
+#      end
+
+      ESCAPE_KEY_CHARS = /[\x00-\x20%\x7F-\xFF]/n
+
+      def self.build_memcached(*addresses)
+        addresses = addresses.flatten
+        options = addresses.extract_options!
+        addresses = ["localhost:11211"] if addresses.empty?
+        Memcached::Rails.new(addresses, options)
+      end
+
+      def initialize(*addresses)
+        addresses = addresses.flatten
+        options = addresses.extract_options!
+        super(options)
+
+        if addresses.first.respond_to?(:get)
+          @data = addresses.first
+        else
+          mem_cache_options = options.dup
+          UNIVERSAL_OPTIONS.each{|name| mem_cache_options.delete(name)}
+          @data = self.class.build_memcached(*(addresses + [mem_cache_options]))
+        end
+
+        extend Strategy::LocalCache
+      end
+
+      # Reads multiple values from the cache using a single call to the
+      # servers for all keys. Options can be passed in the last argument.
+      def read_multi(*names)
+        options = names.extract_options!
+        options = merged_options(options)
+        keys_to_names = Hash[names.map{|name| [escape_key(namespaced_key(name, options)), name]}]
+        raw_values = @data.get_multi(keys_to_names.keys, :raw => true)
+        values = {}
+        raw_values.each do |key, value|
+          entry = deserialize_entry(value)
+          values[keys_to_names[key]] = entry.value unless entry.expired?
+        end
+        values
+      end
+
+      def increment(name, amount = 1, options = nil) # :nodoc:
+        options = merged_options(options)
+        instrument(:increment, name, :amount => amount) do
+          @data.incr(escape_key(namespaced_key(name, options)), amount)
+        end
+      rescue *NONFATAL_EXCEPTIONS => e
+        @data.log_exception(e)
+        nil
+      end
+
+      def decrement(name, amount = 1, options = nil) # :nodoc:
+        options = merged_options(options)
+        instrument(:decrement, name, :amount => amount) do
+          @data.decr(escape_key(namespaced_key(name, options)), amount)
+        end
+      rescue *NONFATAL_EXCEPTIONS => e
+        @data.log_exception(e)
+        nil
+      end
+
+      # Clear the entire cache on all memcached servers. This method should
+      # be used with care when shared cache is being used.
+      def clear(options = nil)
+        @data.flush_all
+      end
+
+      # Get the statistics from the memcached servers.
+      def stats
+        @data.stats
+      end
+
+      protected
+        # Read an entry from the cache.
+        def read_entry(key, options) # :nodoc:
+          deserialize_entry(@data.get(escape_key(key), true))
+        rescue *NONFATAL_EXCEPTIONS => e
+          @data.log_exception(e)
+          nil
+        end
+
+        # Write an entry to the cache.
+        def write_entry(key, entry, options) # :nodoc:
+          method = options && options[:unless_exist] ? :add : :set
+          value = options[:raw] ? entry.value.to_s : entry
+          expires_in = options[:expires_in].to_i
+          if expires_in > 0 && !options[:raw]
+            # Set the memcache expire a few minutes in the future to support race condition ttls on read
+            expires_in += 5.minutes
+          end
+          @data.send(method, escape_key(key), value, expires_in, options[:raw])
+        rescue *NONFATAL_EXCEPTIONS => e
+          @data.log_exception(e)
+          false
+        end
+
+        # Delete an entry from the cache.
+        def delete_entry(key, options) # :nodoc:
+          @data.delete(escape_key(key))
+          true
+        rescue *NONFATAL_EXCEPTIONS => e
+          @data.log_exception(e)
+          false
+        end
+
+      private
+
+        # Memcache keys are binaries. So we need to force their encoding to binary
+        # before applying the regular expression to ensure we are escaping all
+        # characters properly.
+        def escape_key(key)
+          key = key.to_s.dup
+          key = key.force_encoding("BINARY") if key.encoding_aware?
+          key = key.gsub(ESCAPE_KEY_CHARS){ |match| "%#{match.getbyte(0).to_s(16).upcase}" }
+          key = "#{key[0, 213]}:md5:#{Digest::MD5.hexdigest(key)}" if key.size > 250
+          key
+        end
+
+        def deserialize_entry(raw_value)
+          if raw_value
+            entry = Marshal.load(raw_value) rescue raw_value
+            entry.is_a?(Entry) ? entry : Entry.new(entry)
+          else
+            nil
+          end
+        end
+
+    end
+  end
+end
